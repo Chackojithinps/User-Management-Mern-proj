ea5fe172d876dda0c9e4b78f4d7a76212bc8a3f7
Chacko jithin <jithinchackopayyanat@gmail.com>
2023-07-15 00:30:07 +0530

    sighnup page controller code completed

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
 const mongoose = require('mongoose');
+const router = require('./routes/user-router')
 const app = express();
-
+app.use(express.json())
+app.use('/api',router)
 mongoose.connect('mongodb+srv://jithinchackopayyanat:0UbEV9ZpyIYd96Ks@cluster0.jgzemu9.mongodb.net/User-management?retryWrites=true&w=majority').then((res)=>{
     app.listen(5000);
     console.log("db connected")
---
+++ controller/user-controller.js
@@ -0,0 +1,30 @@
+const User = require('../model/user-model')
+
+ 
+const userSignup = async(req,res,next) =>{
+    const {name,email,password} = req.body
+    // console.log(req.body)
+    let isExists;
+    try {
+         isExists = await User.findOne({email:email})
+    } catch (error) {
+        console.log("val "+error.message)
+    }
+    if(isExists){
+        return res.status(401).json({message:"user with this email already exits"})
+    }
+    const user = new User ({
+        name,
+        email,
+        password
+    })
+    try {
+        await user.save()
+    } catch (error) {
+        console.log("signup "+error.message)
+    }
+
+    return res.status(200).json({message:user})
+}
+
+module.exports = {userSignup}
