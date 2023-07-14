604ad99456792c9eecbaaf4c17a64e09b2696c81
Chacko jithin <jithinchackopayyanat@gmail.com>
2023-07-15 00:42:28 +0530

    bcrypt password completed

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,4 +1,5 @@
 const User = require('../model/user-model')
+const bcrypt = require("bcryptjs")
 
  
 const userSignup = async(req,res,next) =>{
@@ -8,20 +9,21 @@ const userSignup = async(req,res,next) =>{
     try {
          isExists = await User.findOne({email:email})
     } catch (error) {
-        console.log("val "+error.message)
+        console.log(error.message)
     }
     if(isExists){
         return res.status(401).json({message:"user with this email already exits"})
     }
+    const hashPassword =await bcrypt.genSalt(password);
     const user = new User ({
         name,
         email,
-        password
+        password:hashPassword
     })
     try {
         await user.save()
     } catch (error) {
-        console.log("signup "+error.message)
+        console.log(error.message)
     }
 
     return res.status(200).json({message:user})
