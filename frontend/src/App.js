33f62b43fc407ed62a80a8aff2c7799c9f134139
Chacko jithin <jithinchackopayyanat@gmail.com>
2023-07-15 23:53:27 +0530

    frontend basic setup completed

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Components/Header'
+import Signup from './Components/Signup'
+
+
+function App() {
+  return (
+    <div >
+        < Header />
+        <Signup />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/Signup.jsx
@@ -0,0 +1,10 @@
+import React from 'react' 
+
+const Signup =()=>{
+    return (
+        <div>
+            This is sighnup page
+        </div>
+    )
+}
+export default Signup
