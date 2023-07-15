ed8f028e60578bf55722c06ce1c93c7272893ec1
Chacko jithin <jithinchackopayyanat@gmail.com>
2023-07-15 11:39:06 +0530

    password hashing completed

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,6 +1,6 @@
 const User = require('../model/user-model')
-const bcrypt = require("bcryptjs")
-
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
  
 const userSignup = async(req,res,next) =>{
     const {name,email,password} = req.body
@@ -14,7 +14,7 @@ const userSignup = async(req,res,next) =>{
     if(isExists){
         return res.status(401).json({message:"user with this email already exits"})
     }
-    const hashPassword =await bcrypt.genSalt(password);
+    const hashPassword =await bcrypt.hash(password.toString(),10);
     const user = new User ({
         name,
         email,
@@ -29,4 +29,28 @@ const userSignup = async(req,res,next) =>{
     return res.status(200).json({message:user})
 }
 
-module.exports = {userSignup}
+const userLogin = async (req,res)=>{
+    const {email,password} = req.body;
+    let isUserExists;
+    try {
+         isUserExists = await User.findOne({email:email})
+         console.log(isUserExists)
+    } catch (error) {
+        console.log(error.message)
+    }
+    if(!isUserExists){
+        return res.status(404).json({message:"user not found"})
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(
+        req.body.password.toString(),
+        isUserExists.password
+      );
+    console.log(isPasswordCorrect)
+    if(!isPasswordCorrect){
+        return res.status(404).json({message:"incorrect password"})
+    }
+    return res.status(200).json({message:"Successfully loggedin"})
+}
+
+module.exports = {userSignup,userLogin}
--- routes/user-router.js
+++ routes/user-router.js
@@ -3,5 +3,6 @@ const userController = require('../controller/user-controller')
 const router = express.Router()
 
 router.post('/signup',userController.userSignup)
+router.post('/login',userController.userLogin)
 
 module.exports = router
